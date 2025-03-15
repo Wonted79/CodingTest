6f58f84b6b1900d18ec31cb69498ffe8c28611a2
leegyeseung <lgs0004237@naver.com>
2025-03-15 20:44:43 +0900

    2025-03-15 기능개발

--- src/Programers/기능개발.java
+++ src/Programers/기능개발.java
@@ -1,28 +1,39 @@
 package Programers;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class 기능개발 {
 
-    public static int[] solution(int[] progresses ,int[] speeds) {
+    public static List<Integer>  solution(int[] progresses ,int[] speeds) {
 
         Queue<Integer> q = new LinkedList<Integer>();
-        int max = 0;
-        int count =0;
+        List<Integer> res = new ArrayList<>();
+
         for (int i = 0; i < progresses.length; i++) {
-            int num = 100-progresses[i]/speeds[i];
+            int num = (100-progresses[i]+speeds[i]-1)/speeds[i];
             q.offer(num);
-
-
-
         }
-        return null;
+
+        int currentMax = q.poll();
+        int count = 1;
+
+        while (!q.isEmpty()) {
+            int day = q.poll();
+            if (day <= currentMax) {
+                count++;
+            } else {
+                res.add(count);
+                count = 1;
+                currentMax = day;
+            }
+        }
+        res.add(count);
+
+        return res;
     }
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(solution(new int[]{93, 30, 55},new int[]{1, 30, 5})));
-        System.out.println(Arrays.toString(solution(new int[]{95, 90, 99, 99, 80, 99},new int[]{1, 1, 1, 1, 1, 1})));
+        System.out.println(solution(new int[]{93, 30, 55},new int[]{1, 30, 5}));
+        System.out.println(solution(new int[]{95, 90, 99, 99, 80, 99},new int[]{1, 1, 1, 1, 1, 1}));
 
     }
 
